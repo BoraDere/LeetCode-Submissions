c39db89dfa3b847b72e20aa2785b60078b55cc94
Bora Dere <boradere@hotmail.com.tr>
2024-08-02 02:12:05 +0300

    Time: 0 ms (100.00%) | Memory: 12 MB (11.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,33 +1,66 @@
+struct Node {
+    Node* child[26];
+    bool isLast;
+    int childCount;
+    Node() {
+        for (int i = 0; i < 26; i++) {
+            child[i] = NULL;
+        }
+        isLast = false;
+        childCount = 0;
+    }
+};
+
+class Trie {
+    private:
+        Node* root;
+    public:
+        Trie() {
+            root = new Node();
+        }
+        void insert(std::string& key) {
+            Node* curr = root;
+            for (int i = 0; i < key.length(); i++) {
+                int index = key[i] - 'a';
+                if (curr->child[index] == NULL) {
+                    curr->child[index] = new Node();
+                    curr->childCount++;
+                }
+                curr = curr->child[index];
+            }
+            curr->isLast = true;
+        }
+        void findPrefix(string& key, string& ans) {
+            Node* curr = root;
+            for (int i = 0; i < key.size(); i++) {
+                int index = key[i] - 'a';
+                if (curr->childCount != 1 || curr->isLast) {
+                    break;
+                }
+                curr = curr->child[index];
+                ans += key[i];
+                
+            }
+        }
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        std::string minStr = "";
-        int minLen = 201;
+        if (strs.empty()) return "";
 
-        for (std::string str : strs) {
-            if (str.length() < minLen) {
-                minLen = str.length();
-                minStr = str;
-            }
+        Trie trie;
+        for (string str : strs) {
+            trie.insert(str);
         }
 
-        for (std::string str : strs) {
-            if (str == minStr) {
-                ;
-            }
-            else {
-                for (int i = 0; i < minLen; i++) {
-                    if (str[i] != minStr[i]) {
-                        minStr = minStr.substr(0, i);
-                        minLen = i;
-                        break;
-                    }
-                }
-            }
-            
-        }
-
-        return minStr;
+        string key = strs[0];
+        string ans = "";
         
+        trie.findPrefix(key, ans);
+        if(ans.size()==0){
+            return "";
+        }
+        return ans;
     }
 };
