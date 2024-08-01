a067bda07ad3e7bf8135bad6081dcea492f02577
Bora Dere <boradere@hotmail.com.tr>
2024-08-02 00:54:01 +0300

    Time: 3 ms (91.86%) | Memory: 17.7 MB (17.69%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int s = 0;
+
+        for (int i = 0; i < details.size(); i++) {
+
+            if (stoi(details[i].substr(11, 2)) > 60) {
+                s++;
+            }
+        }
+
+        return s;
+    }
+};
