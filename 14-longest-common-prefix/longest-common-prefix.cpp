11542051df29560edeeb5f82acbc7ea81e3ebafa
Bora Dere <boradere@hotmail.com.tr>
2024-08-02 10:10:18 +0300

    Time: 3 ms (70.49%) | Memory: 12 MB (11.51%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,7 +1,7 @@
 struct Node {
     Node* child[26];
-    int childCount;
     bool isLast;
+    int childCount;
     Node() {
         for (int i = 0; i < 26; i++) {
             child[i] = NULL;
@@ -14,34 +14,32 @@ struct Node {
 class Trie {
     private:
         Node* root;
-
     public:
         Trie() {
             root = new Node();
         }
-
-        void insert(string& key) {
+        void insert(std::string& key) {
             Node* curr = root;
-            for (int i = 0; i < key.size(); i++) {
+            for (int i = 0; i < key.length(); i++) {
                 int index = key[i] - 'a';
                 if (curr->child[index] == NULL) {
-                    curr->child[index] = new Node;
+                    curr->child[index] = new Node();
                     curr->childCount++;
                 }
-            curr = curr->child[index];
+                curr = curr->child[index];
             }
             curr->isLast = true;
         }
-
         void findPrefix(string& key, string& ans) {
             Node* curr = root;
             for (int i = 0; i < key.size(); i++) {
                 int index = key[i] - 'a';
-                if (curr->isLast || curr->childCount > 1) {
+                if (curr->childCount != 1 || curr->isLast) {
                     break;
                 }
-                ans += key[i];
                 curr = curr->child[index];
+                ans += key[i];
+                
             }
         }
 };
@@ -49,23 +47,20 @@ class Trie {
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) {
-            return "";
-        }
+        if (strs.empty()) return "";
 
         Trie trie;
         for (string str : strs) {
             trie.insert(str);
         }
 
-        string ans = "";
         string key = strs[0];
+        string ans = "";
+        
         trie.findPrefix(key, ans);
-
         if(ans.size()==0){
             return "";
         }
-
         return ans;
     }
 };
