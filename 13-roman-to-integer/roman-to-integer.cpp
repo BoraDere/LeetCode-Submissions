fd6e9609e4f8b4196936f08268e640e5aa5e75e5
Bora Dere <boradere@hotmail.com.tr>
2024-08-04 16:01:28 +0300

    Time: 12 ms (47.08%) | Memory: 12.8 MB (42.65%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int res = 0;
+        unordered_map<char, int> table = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c == 'I' && i+1 < s.size() && s[i+1] == 'V') {
+                res += 4;
+                i++;
+            }
+            else if (c == 'I' && i+1 < s.size() && s[i+1] == 'X') {
+                res += 9;
+                i++;
+            }
+            else if (c == 'X' && i+1 < s.size() && s[i+1] == 'L') {
+                res += 40;
+                i++;
+            }
+            else if (c == 'X' && i+1 < s.size() && s[i+1] == 'C') {
+                res += 90;
+                i++;
+            }
+            else if (c == 'C' && i+1 < s.size() && s[i+1] == 'D') {
+                res += 400;
+                i++;
+            }
+            else if (c == 'C' && i+1 < s.size() && s[i+1] == 'M') {
+                res += 900;
+                i++;
+            }
+            else {
+                res += table[c];
+            }
+        }
+        return res;
+    }
+};
