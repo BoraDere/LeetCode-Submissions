6144cbac64cfe969aa6c90f312a8bdc88ddd0519
Bora Dere <boradere@hotmail.com.tr>
2024-08-04 16:02:23 +0300

    Time: 11 ms (53.48%) | Memory: 13 MB (32.37%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -17,26 +17,32 @@ public:
             if (c == 'I' && i+1 < s.size() && s[i+1] == 'V') {
                 res += 4;
                 i++;
+                continue;
             }
             else if (c == 'I' && i+1 < s.size() && s[i+1] == 'X') {
                 res += 9;
                 i++;
+                continue;
             }
             else if (c == 'X' && i+1 < s.size() && s[i+1] == 'L') {
                 res += 40;
                 i++;
+                continue;
             }
             else if (c == 'X' && i+1 < s.size() && s[i+1] == 'C') {
                 res += 90;
                 i++;
+                continue;
             }
             else if (c == 'C' && i+1 < s.size() && s[i+1] == 'D') {
                 res += 400;
                 i++;
+                continue;
             }
             else if (c == 'C' && i+1 < s.size() && s[i+1] == 'M') {
                 res += 900;
                 i++;
+                continue;
             }
             else {
                 res += table[c];
